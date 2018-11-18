ad9328008f59a9e0636d31e76aa3a68fbe7f7130
Tanjic <45147677+Tanjic@users.noreply.github.com>
2018-11-18 17:51:01 +0100

    First commit

---
+++ headers/Arrays.h
@@ -0,0 +1,9 @@
+#ifndef _arrays_h_
+//funkcija koja proverava da li ovaj heder fajl vec postoji
+#define _arrays_h
+
+void readArrayElements(int arr[], int arrLength);
+void printArrayElements(int arr[], int arrLength);
+#endif
+
+
---
+++ headers/sortalgs.h
@@ -0,0 +1,7 @@
+#ifndef _sortalgs_h_
+
+#define _sortalgs_h_
+
+void bubbleSort( int arr[],int arrLength);
+
+#endif
